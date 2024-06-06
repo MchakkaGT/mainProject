b8e9f3f2b069e2de5d238bffb482642f64a17911
MchakkaGT <mchakka6@gatech.edu>
2024-06-06 15:20:12 -0400

    added map directory to be able to access the gogole map with /map

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
+    path('map/', include('map.urls'))
 ]
+
+def map_view(request):
+    return render(request, 'map/map.html')
