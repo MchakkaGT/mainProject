62241668ecda1d3705cfe43b55450f978b104b4f
rte23 <149116239+rte23@users.noreply.github.com>
2024-07-18 15:22:06 -0400

    updated color palette

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,9 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
