d6374c6ec8a11eab359a61b5e465e848ad0ec09f
MchakkaGT <mchakka6@gatech.edu>
2024-07-23 17:29:28 -0400

    added url changes

--- map/urls.py
+++ map/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+
+
+app_name = "map"
+
 
 urlpatterns = [
-    path('', views.map_view, name='map_view'),
+   path('', views.map_view, name='map_view'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
