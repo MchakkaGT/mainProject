7c0c87c1fae721d021417b361b65353bfb80a90a
MchakkaGT <mchakka6@gatech.edu>
2024-07-24 20:09:23 -0400

    added more static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'polls')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'polls/static/polls')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
